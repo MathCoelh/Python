f6607bcf3e43e691b2de975640628d282380b15c
Matheus Coelho Alves Silva <91644967+MathCoelh@users.noreply.github.com>
2022-07-26 11:16:49 -0300

    Delete ex028.py

--- ex028.py
+++
@@ -1,8 +0,0 @@
-import random
-n = int(input("""Olá, acabei de pensar em um número, vamos ver se você adivinha?"
-Digite o número que você acha que eu pensei!: """))
-numero = random.randint(0, 5)
-if n == numero:
-    print('PARABÉNS VOCÊ GANHOU!')
-else:
-    print('O COMPUTADOR GANHOU!')
