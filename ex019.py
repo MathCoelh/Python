0f1a63825e40168325421055c4fa37715c3f0bd7
Matheus Coelho Alves Silva <91644967+MathCoelh@users.noreply.github.com>
2022-07-26 11:14:23 -0300

    Delete ex019.py

--- ex019.py
+++
@@ -1,8 +0,0 @@
-import random
-a = input('Digite o Primeiro nome:')
-b = input('Digite o Segundo nome:')
-c = input('Digite o Terceiro nome:')
-d = input('Digite o Quarto nome:')
-list = [a,b,c,d]
-r = random.choice(list)
-print('O escolhido foi: {}'.format(r))
