c3e6f19959e7cfe5776bc7cd31fc53a65dc78e71
Matheus Coelho Alves Silva <91644967+MathCoelh@users.noreply.github.com>
2022-07-26 11:10:42 -0300

    Delete ex004.py

--- ex004.py
+++
@@ -1,6 +0,0 @@
-algo = input('Digite algo:')
-print('O tipo primitivo é', type(algo))
-print('Só tem espaços?', algo.isspace())
-print('É um número?', algo.isnumeric())
-print('É um alfabeto?', algo.isalpha())
-print('É alfanumérico?', algo.isalnum())
