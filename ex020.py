836b30cc9300596ece85f05241b591af2a79a47e
Matheus Coelho Alves Silva <91644967+MathCoelh@users.noreply.github.com>
2022-07-26 11:14:37 -0300

    Delete ex020.py

--- ex020.py
+++
@@ -1,8 +0,0 @@
-import random
-a = input('Digite o Primeiro nome:')
-b = input('Digite o Segundo nome:')
-c = input('Digite o Terceiro nome:')
-d = input('Digite o Quarto nome:')
-list = [a,b,c,d]
-random.shuffle(list)
-print('A ordem de apresentação é {}'.format(list))
