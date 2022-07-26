0b4cf69c5f451bc1be96f8c0214d8215da83409b
Matheus Coelho Alves Silva <91644967+MathCoelh@users.noreply.github.com>
2022-07-26 11:17:46 -0300

    Delete ex032.py

--- ex032.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-ano = int(input('Qual ano deseja analisar? Coloque 0 para analizar o ano atual: '))
-if ano == 0:
-    ano = date.today().year
-if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
-    print('O ano {} é Bissexto'.format(ano))
-else:
-    print('O ano {} não é Bissexto'.format(ano))
