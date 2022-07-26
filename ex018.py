c7abad14cfc6cc22d56e85f1a68d991e1f89fc65
Matheus Coelho Alves Silva <91644967+MathCoelh@users.noreply.github.com>
2022-07-26 11:14:09 -0300

    Delete ex018.py

--- ex018.py
+++
@@ -1,6 +0,0 @@
-import math
-a = float(input('Digite o número do ângulo: '))
-s = math.sin(math.radians(a))
-c = math.cos(math.radians(a))
-t = math.tan(math.radians(a))
-print('O sen é {:.2f}, o cos é {:.2f} e o tang é {:.2f}!'.format(s,c,t))
