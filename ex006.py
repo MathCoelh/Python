e7b976ed65d69129180571609b67f3185b05b1f5
Matheus Coelho Alves Silva <91644967+MathCoelh@users.noreply.github.com>
2022-07-26 11:11:23 -0300

    Delete ex006.py

--- ex006.py
+++
@@ -1,5 +0,0 @@
-n = int(input('Digite um número: '))
-d = n * 2
-t = n * 3
-rq = n ** (1/2)
-print('O dobro do número que você escolheu é {}, o trípulo é {} e a raíz quadrada é {:0.3f}'.format(d,t,rq))
