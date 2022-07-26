a2ee65de18f818fdcf1d053e0a7433407fa45fea
Matheus Coelho Alves Silva <91644967+MathCoelh@users.noreply.github.com>
2022-07-26 11:11:37 -0300

    Delete ex007.py

--- ex007.py
+++
@@ -1,5 +0,0 @@
-nota1 = float(input('Digite sua primeira nota: '))
-nota2 = float(input('Digite sua segunda nota: '))
-multi= nota1 + nota2
-media= multi / 2
-print('A sua média é {}!'.format(media))
